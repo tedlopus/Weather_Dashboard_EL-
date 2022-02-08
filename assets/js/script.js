fb1fe5f3c0b850b4ae2e564cfe65557d5ae49c1a
tedlopus <tedlopus@gmail.com>
2022-02-07 23:58:02 -0700

    updated css and added moment and 5day forecast functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,68 +5,80 @@ var forecast = document.getElementsByClassName("col-forecast");
 var currentCity = document.getElementById("current-city");
 var cities = [];
 var search = document.getElementById("search");
+// Moment function that creates the current date to base the 5 day forecast off of
+var today = moment().format("MMMM Do YYYY");
 
-var URL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={ddfab919ab7a8eb5c76ce05898c85d7f}`
-var apiKey = 'ddfab919ab7a8eb5c76ce05898c85d7f';
+var URL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={ddfab919ab7a8eb5c76ce05898c85d7f}`;
+var apiKey = "ddfab919ab7a8eb5c76ce05898c85d7f";
 //create new variables if needed for js
 var inputSubmitHandler = function () {
-    //event.preventDefault();
-  
-    var inputCity = document.getElementById("cityText").value;
-    console.log(inputCity, "City");
-    if (inputCity) {
-      getLongLat(inputCity);
-      document.getElementById("cityText").value = '';
-    } else {
-      alert('Please enter a valid city');
-    }
-  };
+  //event.preventDefault();
 
-  function getLongLat (cityName) {
-    URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-    fetch(URL)
-    .then(response =>{
-        return response.json()
-    }).then(apidata=>{
-        console.log(apidata);
-        var long = apidata.coord.lon
-        var lat =apidata.coord.lat
-        document.getElementById('cityName').textContent = cityName
-        getForecast(lat,long)
-    })
-
+  var inputCity = document.getElementById("cityText").value;
+  console.log(inputCity, "City");
+  if (inputCity) {
+    getLongLat(inputCity);
+    document.getElementById("cityText").value = "";
+  } else {
+    alert("Please enter a valid city");
   }
+};
+//Fetches the data based of of the city name entered
+function getLongLat(cityName) {
+  URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  fetch(URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((apidata) => {
+      console.log(apidata);
+      var long = apidata.coord.lon;
+      var lat = apidata.coord.lat;
+      document.getElementById("cityName").textContent = cityName;
+      getForecast(lat, long);
+      getFiveDayForecast(lat, long);
+    });
+}
 
-
-  function getForecast (lat, long) {
-    URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,alerts,minutely&appid=${apiKey}&units=imperial`
-    fetch(URL)
-    .then(response =>{
-        return response.json()
-    }).then(apidata=>{
-        console.log(apidata);
-        var forecast = `<h4>Description:${apidata.current.weather[0].description}.
+//This prints the current date forecast for the city inputted for the current city info: city name, the date, an icon represtantion of the weather, the temp, the humidity, and wind speed
+function getForecast(lat, long) {
+  URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,alerts,minutely&appid=${apiKey}&units=imperial`;
+  fetch(URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((apidata) => {
+      console.log(apidata);
+      var forecast = ` <h2>${today}<h2> <br><h4> Description: ${apidata.current.weather[0].description}
         <img src="https://openweathermap.org/img/wn/${apidata.current.weather[0].icon}@2x.png" /></h4>
-        <h5>Wind speed:${apidata.wind_speed}</h5>
-        <h5>Humidity:${apidata.humidity}</h5>
-        `
-        document.getElementById("current-forecast").innerHTML= forecast
+        <br><h5> Temperature: ${apidata.current.temp}</h5>
+        <br><h5> Wind speed: ${apidata.current.wind_speed}</h5>
+        <br><h5> Humidity: ${apidata.current.humidity}</h5>
+        <br><h5> UV Index: ${apidata.current.uvi}
+        `;
+      document.getElementById("current-forecast").innerHTML = forecast;
+    });
+}
+
+//API url is working but it needs to be linked to the divs id'ed 1 to 5
+function getFiveDayForecast(lat, long) {
+  URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
+  fetch(URL)
+    .then((response) => {
+      return response.json();
     })
-  }
-//Going to need to create a way for the current date to populate using moment and getting the next 5 days after that
-
-
-
-//dynamically creating elements for the current city info: city name, the date, an icon represtantion of the weather, the temp, the humidity, and wind speed
-
-
-
-//create a function the pulls information from API to html and fills container
-
-
-
-//create a function that pulls information from API and shows the 5-day forecast cards
-
+    .then((apidata) => {
+      console.log(apidata);
+      //var forecast = ` <h2>${today}<h2> <br><h4> Description: ${apidata.current.weather[0].description}
+      //<img src="https://openweathermap.org/img/wn/${apidata.current.weather[0].icon}@2x.png" /></h4>
+      //<br><h5> Temperature: ${apidata.current.temp}</h5>
+      //<br><h5> Wind speed: ${apidata.current.wind_speed}</h5>
+      //<br><h5> Humidity: ${apidata.current.humidity}</h5>
+      //<br><h5> UV Index: ${apidata.current.uvi}
+      //`
+      //document.getElementById("current-forecast").innerHTML= forecast
+    });
+}
 
 //create a function for saving local storage for each city searched
-search.addEventListener('click', inputSubmitHandler);
+search.addEventListener("click", inputSubmitHandler);
